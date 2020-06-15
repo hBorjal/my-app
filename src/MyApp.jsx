71ccc61864c3cf2bb67402f43ccec56c6fe1b2b9
Horacio Borja <horacio.borja.93@gmail.com>
2020-06-15 16:19:36 -0500

    Firts commit

---
+++ src/MyApp.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ShellBar, ShellBarItem, Avatar } from '@ui5/webcomponents-react';
+import '@ui5/webcomponents-icons/dist/icons/add.js';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Home } from './Home';
+import { Detail } from './Detail';
+import { useHistory } from 'react-router-dom';
+
+export function MyApp() {
+	const history = useHistory();
+	const handleLogoClick = () => {
+		history.push('./');
+	};
+	return (
+		<div>
+			<ShellBar
+				onLogoClick={handleLogoClick}
+				logo={
+					'https://raw.githubusercontent.com/SAPDocuments/Tutorials/master/tutorials/ui5-webcomponents-react-dashboard/reactLogo.png'
+				}
+				profile={
+					<Avatar image="https://raw.githubusercontent.com/SAPDocuments/Tutorials/master/tutorials/ui5-webcomponents-react-dashboard/profilePictureExample.png" />
+				}
+				primaryTitle={'My App'}
+			>
+				<ShellBarItem src="sap-icon://add" text="Add" />
+			</ShellBar>
+			<Switch>
+				<Route path="/home" component={Home} />
+				<Route path="/detail" component={Detail} />
+				<Redirect from="/" to="/home" />
+			</Switch>
+		</div>
+	);
+}
